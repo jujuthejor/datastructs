a4f2fa80773c6b4c13a68e448c4940f965ba8fd3
Jose Gabriel Ramos <jose.gabriel.ramos.93@gmail.com>
2021-09-16 22:19:26 +0800

    moved tests into their own functions

--- main.c
+++ main.c
@@ -19,6 +19,10 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+#ifdef __MINGW64__
+#define __USE_MINGW_ANSI_STDIO 1
+#endif
+
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -26,8 +30,16 @@
 #include "stack.h"
 
 void test_list();
+void test_stack();
 
 int main(void)
+{
+	test_list();
+	//test_stack();
+	return 0;
+}
+
+void test_stack()
 {
 	Stack *testStack;
 	if(initStack(&testStack)) {
@@ -35,24 +47,32 @@ int main(void)
 		exit(EXIT_FAILURE);
 	}
 
-	printf("Size of stack: %d\n", sizeOfStack(&testStack));
+	printf("Size of stack: %zu\n", sizeOfStack(&testStack));
 	if (isEmptyStack(&testStack)) {
 		printf("Stack is empty\n");
 	}
 	printf("Pushing elements to stack...\n");
 	pushStack(&testStack, 69);
 	pushStack(&testStack, 420);
-	printf("Size of stack right now: %d\n", sizeOfStack(&testStack));
-	printf("Stack peek: %d\nDeleting...\n", peekStack(&testStack));
-	popStack(&testStack);
-	printf("Size of stack right now: %d\n", sizeOfStack(&testStack));
-	printf("Stack peek: %d\n", peekStack(&testStack));
+	pushStack(&testStack, 3);
+	pushStack(&testStack, 4);
+	pushStack(&testStack, 77);
+
+	while(!isEmptyStack(&testStack)) {
+		printf("Size of stack right now: %zu\n", (size_t)sizeOfStack(&testStack));
+		printf("Stack peek: %d\nDeleting...\n", peekStack(&testStack));
+		if (!isEmptyStack(&testStack)) {
+			popStack(&testStack);
+		}
+	}
+
+	printf("Size of stack right now: %zu\n", (size_t)sizeOfStack(&testStack));
+
 	if (isEmptyStack(&testStack)) {
 		printf("Stack is empty\n");
 	}
 
 	freeStack(&testStack);
-	return 0;
 }
 
 void test_list()
@@ -63,44 +83,52 @@ void test_list()
 		exit(EXIT_FAILURE);
 	}
 
-	printf("Size of list: %d\n", sizeofList(&testList));
+	printf("Size of list: %zu\n", sizeofList(&testList));
 
 	addToList(&testList, 69);
 	addToList(&testList, 420);
 	addToList(&testList, 86);
 	addToList(&testList, 39);
 	printList(&testList);
-	printf("Size of list: %d\n", sizeofList(&testList));
+	printf("Size of list: %zu\n", sizeofList(&testList));
 
 	insertIntoList(&testList, 3, 33);
 	insertIntoList(&testList, 0, 44);
 	printList(&testList);
-	printf("Size of list: %d\n", sizeofList(&testList));
+	printf("Size of list: %zu\n", sizeofList(&testList));
 
 	insertIntoList(&testList, 5, 58008);
 	insertIntoList(&testList, 6, 1337);
 	printList(&testList);
-	printf("Size of list: %d\n", sizeofList(&testList));
+	printf("Size of list: %zu\n", sizeofList(&testList));
 
 	int in_index = 0;
 	do {
 		printf("Enter index (-1 to finish): ");
-		scanf("%d", &in_index);
-		if(in_index >= 0 && in_index < sizeofList(&testList)) {
+		(void) !scanf("%d", &in_index); 															// typecast to void so -Wunused-result won't happen
+		if(in_index >= 0 && (size_t)in_index < sizeofList(&testList)) {
 			printf("Value at index %d is: %d\n", in_index, getFromList(&testList, in_index));
-		} else if (in_index >= sizeofList(&testList)){
+		} else if (in_index >= (int)sizeofList(&testList)){
 			printf("Please enter a value within the valid range\n");
 		}
 	} while(in_index >= 0);
-//	getFromList(&testList, 100);
+//	getFromList(&testList, 100);	// this is intentionally an error
 
 	printList(&testList);
 	deleteFromList(&testList, 4);
 	printList(&testList);
+	printf("Size of list: %zu\n", sizeofList(&testList));
 	deleteFromList(&testList, 0);
 	printList(&testList);
+	printf("Size of list: %zu\n", sizeofList(&testList));
 	deleteFromList(&testList, sizeofList(&testList)-1);
 	printList(&testList);
+	printf("Size of list: %zu\n", sizeofList(&testList));
 
+	while(sizeofList(&testList) != 0) {
+		deleteFromList(&testList, 0);
+		printList(&testList);
+		printf("Size of list: %zu\n", sizeofList(&testList));
+	}
 	freeList(&testList);
 }
