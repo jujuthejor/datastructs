dd8eab4c8c015babdb55b6a81e9ee99182127535
Jose Gabriel Ramos <jose.gabriel.ramos.93@gmail.com>
2021-09-16 22:35:24 +0800

    changed logic of determining size and some other debugging

--- stack.c
+++ stack.c
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2021 Jose Gabriel Ramos (@jujuthejor)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,6 +31,7 @@ struct st_node {
 
 struct stack {
 	struct st_node *head, *tail;
+	size_t size;
 };
 
 int initStack(Stack **stackptr)
@@ -20,25 +42,16 @@ int initStack(Stack **stackptr)
 		return -1;
 	}
 
+	(*stackptr)->size = 0;
 	(*stackptr)->head = NULL;
 	(*stackptr)->tail = NULL;
 
 	return 0;
 }
 
-int sizeOfStack(Stack **stackptr)
+size_t sizeOfStack(Stack **stackptr)
 {
-	struct st_node *traverse = (*stackptr)->head;
-	int count = 0;
-
-	if(traverse != NULL) {
-		while(traverse != NULL) {
-			count++;
-			traverse = traverse->next;
-		}
-	}
-
-	return count;
+	return (*stackptr)->size;
 }
 
 int pushStack(Stack **stackptr, int value)
@@ -63,6 +76,7 @@ int pushStack(Stack **stackptr, int value)
 	}
 
 	(*stackptr)->tail = newElement;
+	(*stackptr)->size++;
 
 	return 0;
 }
@@ -80,24 +94,32 @@ int peekStack(Stack **stackptr)
 void popStack(Stack **stackptr)
 {
 	struct st_node *traverse = (*stackptr)->head;
+	struct st_node *newTail = NULL;
 
 	if(isEmptyStack(stackptr)) {
 		printf("Error: stack empty\n");
 		exit(EXIT_FAILURE);
 	}
 
-	while (traverse->next != (*stackptr)->tail) {
-		traverse = traverse->next;
+	if ((*stackptr)->head == (*stackptr)->tail) {
+		(*stackptr)->head = NULL;
+		(*stackptr)->tail = NULL;
+	} else {
+		while (traverse->next != NULL) {
+			newTail  = traverse;
+			traverse = traverse->next;
+		}
+		newTail->next = NULL;
+		(*stackptr)->tail = newTail;
 	}
-	traverse->next = NULL;
 
-	free((*stackptr)->tail);
-	(*stackptr)->tail = traverse;
+	free(traverse);
+	(*stackptr)->size--;
 }
 
 bool isEmptyStack(Stack **stackptr)
 {
-	if((*stackptr)->head == NULL) {
+	if((*stackptr)->size == 0) {
 		return true;
 	} else {
 		return false;
@@ -121,16 +143,15 @@ void printStack(Stack **stackptr)
 
 void freeStack(Stack **stackptr)
 {
-	struct st_node *traverse = (*stackptr)->head;
-	struct st_node *target = traverse;
+//	struct st_node *traverse = (*stackptr)->head;
+//	struct st_node *target = traverse;
 
-	if (traverse != NULL) {
-		while (traverse->next != NULL) {
-			free(target);
-			traverse = traverse->next;
-			target = traverse;
+//	if (traverse != NULL) {
+//		while (traverse->next != NULL) {
+		while (!isEmptyStack(stackptr)) {
+			popStack(stackptr);
 		}
-	}
+//	}
 
 	free(*stackptr);
 }
